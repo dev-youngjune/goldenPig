ccb974dc74b93126db45ad6fb270d1f135dfc63d
LeeJunSang <dev.enjoy.art@gmail.com>
2023-02-23 17:51:05 +0900

    배너 버튼 이동 제외 js 구현 완료

--- WebContent/assets/js/banner/banner.js
+++ WebContent/assets/js/banner/banner.js
@@ -3,7 +3,7 @@
  */
 
 let $bullets = $(".swiper-pagination label");
-let $spans = $(".swiper-pagination label span");
+let $spans = $(".swiper-pagination span");
 /*
 $bullets.on("click", function(){
 	var $bullet = $bullets.index($(this));
@@ -12,12 +12,25 @@ $bullets.on("click", function(){
 	console.log($(this));
 });*/
 
-// label 클릭 시 이벤트 핸들러 등록
-$bullets.click(function() {
+/*// label 클릭 시 이벤트 핸들러 등록
+$spans.click(function() {
 	// 모든 span tag의 class를 swiper-pagination-bullet으로 설정
 	$spans.removeClass('swiper-pagination-bullet-active').addClass('swiper-pagination-bullet');
 	
 	// 클릭한 label 안의 span tag에 swiper-pagination-bullet-active class 추가
-	$(this).find('span').addClass('swiper-pagination-bullet-active');
+	$(this).addClass('swiper-pagination-bullet-active');
+});*/
+
+
+// swiper 객체 생성
+var mySwiper = new Swiper('.swiper-container', {
+	// 옵션 설정
+	loop: true, // 무한 루프
+	pagination: {
+	  el: '.swiper-pagination', // 페이지네이션 표시 위치
+	},
+	autoplay: {
+	  delay: 2000, // 자동 재생 시간 간격 (3초)
+	},
 });
 
