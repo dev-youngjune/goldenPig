534bcd895e1592fb746c776cd4f0d97df0e6330e
dev-Sungyu <tjfdhs364@gmail.com>
2023-02-26 16:18:45 +0900

    오류 수정

--- src/com/goldenPig/member/MemberJoinOkController.java
+++ src/com/goldenPig/member/MemberJoinOkController.java
@@ -8,12 +8,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.goldenPig.Action;
 import com.goldenPig.Result;
+import com.goldenPig.member.dao.MemberDAO;
+import com.goldenPig.member.domain.MemberVO;
 
 public class MemberJoinOkController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		return null;
+		MemberDAO memberDAO = new MemberDAO();
+		MemberVO  memberVO = new MemberVO();
+		Result result = new Result(); 
+		
+		memberVO.setMemberEmail(req.getParameter("memberEmail"));
+		memberVO.setMemberPassword(req.getParameter("memberPassword"));
+		memberVO.setMemberName(req.getParameter("memberName"));
+		memberVO.setMemberNickName(req.getParameter("memberNickName"));
+		memberVO.setMemberPhoneNumber(req.getParameter("memberPhoneNumber"));
+		memberVO.setMemberBirth(req.getParameter("memberBirthYear")+ req.getParameter("memberBirthMonth") + req.getParameter("memberBirthday"));
+		
+		memberDAO.join(memberVO);
+		
+		result.setPath(req.getContextPath() + "/login.member");
+		result.setRedirect(true);
+		
+		return result;
 	}
 
 }
