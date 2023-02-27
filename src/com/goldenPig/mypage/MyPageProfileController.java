1e3cd4fda9d85da576f88883f8aba985d9f21731
code-leesooncheol <2sooncheol@gmail.com>
2023-02-27 10:58:10 +0900

    커밋 수정

--- src/com/goldenPig/mypage/MyPageProfileController.java
+++ src/com/goldenPig/mypage/MyPageProfileController.java
@@ -1,5 +1,20 @@
 package com.goldenPig.mypage;
 
-public class MyPageProfileController {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.goldenPig.Action;
+import com.goldenPig.Result;
+
+public class MyPageProfileController implements Action {
+	
+	 @Override
+	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		 
+		return null;
+	}
 
 }
