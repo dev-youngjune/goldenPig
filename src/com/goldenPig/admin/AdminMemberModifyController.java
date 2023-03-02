caca9fc8d6e476785a54eb64b2ebe4ce7e1837a7
jeffy1217 <younjune1217@gmail.com>
2023-03-02 13:11:54 +0900

    memberModifyController

--- src/com/goldenPig/admin/AdminMemberModifyController.java
+++ src/com/goldenPig/admin/AdminMemberModifyController.java
@@ -1,20 +1,33 @@
 package com.goldenPig.admin;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+
 import com.goldenPig.Action;
 import com.goldenPig.Result;
+import com.goldenPig.admin.dao.AdminDAO;
+import com.goldenPig.member.dao.MemberDAO;
 
 public class AdminMemberModifyController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		return null;
+		AdminDAO adminDAO = new AdminDAO();
+		MemberDAO memberDAO = new MemberDAO();
+
+		Result result = new Result();
+		JSONArray jsons = new JSONArray();
+		
+		Map<String, Object> adminMemberModifyMap = new HashMap<String, Object>();
+		
+		
 	}
 
 }
