253f5a8ecb4913acd63079878346fd38d45d1db3
dev-Sungyu <tjfdhs364@gmail.com>
2023-03-02 17:47:21 +0900

    DTO 최종 수정

--- src/com/goldenPig/board/domain/BoardDTO.java
+++ src/com/goldenPig/board/domain/BoardDTO.java
@@ -8,10 +8,31 @@ public class BoardDTO {
 	private String boardRegisterDate;
 	private String boardUpdateDate;
 	private Long ReadCount;
+	private Long boardImgId;
+	private String boardImgPath;
+	private String boardImgName;
+	private Long boardLikeId;
+	private Long replyId;
+	private String replyContent;
+	private String replyRegisterDate;
+	private String replyUpdateDate;
+	private Long tagId;
+	private String tagName;
+	private Long favoriteId;
+	private Long answerId;
+	private String answerTitle;
+	private String answerContent;
+	private String answerRegisterDate;
+	private String answerUpdateDate;
+	private Long answerLikeId;
+	
+	
 
 	
 	
 	public BoardDTO() {;}
+
+
 
 
 
@@ -21,9 +42,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setBoardId(Long boardId) {
 		this.boardId = boardId;
 	}
+
+
 
 
 
@@ -33,9 +58,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setMemberId(Long memberId) {
 		this.memberId = memberId;
 	}
+
+
 
 
 
@@ -45,9 +74,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setBoardTitle(String boardTitle) {
 		this.boardTitle = boardTitle;
 	}
+
+
 
 
 
@@ -57,9 +90,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setBoardContent(String boardContent) {
 		this.boardContent = boardContent;
 	}
+
+
 
 
 
@@ -69,9 +106,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setBoardRegisterDate(String boardRegisterDate) {
 		this.boardRegisterDate = boardRegisterDate;
 	}
+
+
 
 
 
@@ -81,9 +122,13 @@ public class BoardDTO {
 
 
 
+
+
 	public void setBoardUpdateDate(String boardUpdateDate) {
 		this.boardUpdateDate = boardUpdateDate;
 	}
+
+
 
 
 
@@ -93,9 +138,285 @@ public class BoardDTO {
 
 
 
+
+
 	public void setReadCount(Long readCount) {
 		ReadCount = readCount;
 	}
+
+
+
+
+
+	public Long getBoardImgId() {
+		return boardImgId;
+	}
+
+
+
+
+
+	public void setBoardImgId(Long boardImgId) {
+		this.boardImgId = boardImgId;
+	}
+
+
+
+
+
+	public String getBoardImgPath() {
+		return boardImgPath;
+	}
+
+
+
+
+
+	public void setBoardImgPath(String boardImgPath) {
+		this.boardImgPath = boardImgPath;
+	}
+
+
+
+
+
+	public String getBoardImgName() {
+		return boardImgName;
+	}
+
+
+
+
+
+	public void setBoardImgName(String boardImgName) {
+		this.boardImgName = boardImgName;
+	}
+
+
+
+
+
+	public Long getBoardLikeId() {
+		return boardLikeId;
+	}
+
+
+
+
+
+	public void setBoardLikeId(Long boardLikeId) {
+		this.boardLikeId = boardLikeId;
+	}
+
+
+
+
+
+	public Long getReplyId() {
+		return replyId;
+	}
+
+
+
+
+
+	public void setReplyId(Long replyId) {
+		this.replyId = replyId;
+	}
+
+
+
+
+
+	public String getReplyContent() {
+		return replyContent;
+	}
+
+
+
+
+
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+
+
+
+
+	public String getReplyRegisterDate() {
+		return replyRegisterDate;
+	}
+
+
+
+
+
+	public void setReplyRegisterDate(String replyRegisterDate) {
+		this.replyRegisterDate = replyRegisterDate;
+	}
+
+
+
+
+
+	public String getReplyUpdateDate() {
+		return replyUpdateDate;
+	}
+
+
+
+
+
+	public void setReplyUpdateDate(String replyUpdateDate) {
+		this.replyUpdateDate = replyUpdateDate;
+	}
+
+
+
+
+
+	public Long getTagId() {
+		return tagId;
+	}
+
+
+
+
+
+	public void setTagId(Long tagId) {
+		this.tagId = tagId;
+	}
+
+
+
+
+
+	public String getTagName() {
+		return tagName;
+	}
+
+
+
+
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+
+
+
+
+	public Long getFavoriteId() {
+		return favoriteId;
+	}
+
+
+
+
+
+	public void setFavoriteId(Long favoriteId) {
+		this.favoriteId = favoriteId;
+	}
+
+
+
+
+
+	public Long getAnswerId() {
+		return answerId;
+	}
+
+
+
+
+
+	public void setAnswerId(Long answerId) {
+		this.answerId = answerId;
+	}
+
+
+
+
+
+	public String getAnswerTitle() {
+		return answerTitle;
+	}
+
+
+
+
+
+	public void setAnswerTitle(String answerTitle) {
+		this.answerTitle = answerTitle;
+	}
+
+
+
+
+
+	public String getAnswerContent() {
+		return answerContent;
+	}
+
+
+
+
+
+	public void setAnswerContent(String answerContent) {
+		this.answerContent = answerContent;
+	}
+
+
+
+
+
+	public String getAnswerRegisterDate() {
+		return answerRegisterDate;
+	}
+
+
+
+
+
+	public void setAnswerRegisterDate(String answerRegisterDate) {
+		this.answerRegisterDate = answerRegisterDate;
+	}
+
+
+
+
+
+	public String getAnswerUpdateDate() {
+		return answerUpdateDate;
+	}
+
+
+
+
+
+	public void setAnswerUpdateDate(String answerUpdateDate) {
+		this.answerUpdateDate = answerUpdateDate;
+	}
+
+
+
+
+
+	public Long getAnswerLikeId() {
+		return answerLikeId;
+	}
+
+
+
+
+
+	public void setAnswerLikeId(Long answerLikeId) {
+		this.answerLikeId = answerLikeId;
+	}
+
+
 
 
 
@@ -103,37 +424,16 @@ public class BoardDTO {
 	public String toString() {
 		return "BoardDTO [boardId=" + boardId + ", memberId=" + memberId + ", boardTitle=" + boardTitle
 				+ ", boardContent=" + boardContent + ", boardRegisterDate=" + boardRegisterDate + ", boardUpdateDate="
-				+ boardUpdateDate + ", ReadCount=" + ReadCount + "]";
+				+ boardUpdateDate + ", ReadCount=" + ReadCount + ", boardImgId=" + boardImgId + ", boardImgPath="
+				+ boardImgPath + ", boardImgName=" + boardImgName + ", boardLikeId=" + boardLikeId + ", replyId="
+				+ replyId + ", replyContent=" + replyContent + ", replyRegisterDate=" + replyRegisterDate
+				+ ", replyUpdateDate=" + replyUpdateDate + ", tagId=" + tagId + ", tagName=" + tagName + ", favoriteId="
+				+ favoriteId + ", answerId=" + answerId + ", answerTitle=" + answerTitle + ", answerContent="
+				+ answerContent + ", answerRegisterDate=" + answerRegisterDate + ", answerUpdateDate="
+				+ answerUpdateDate + ", answerLikeId=" + answerLikeId + "]";
 	}
 
 
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((boardId == null) ? 0 : boardId.hashCode());
-		return result;
-	}
-
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BoardDTO other = (BoardDTO) obj;
-		if (boardId == null) {
-			if (other.boardId != null)
-				return false;
-		} else if (!boardId.equals(other.boardId))
-			return false;
-		return true;
-	}
 
 	
 }
