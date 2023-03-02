b719ae80fcf54a7685db2144cc32fdce08bf5d08
jeffy1217 <younjune1217@gmail.com>
2023-03-03 01:59:35 +0900

    작업을 많이 하지는 않았지만 날아가니 마음은 아프네요..

--- src/com/goldenPig/admin/AdminMemberModifyController.java
+++ src/com/goldenPig/admin/AdminMemberModifyController.java
@@ -1,33 +1,26 @@
 package com.goldenPig.admin;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
-
 import com.goldenPig.Action;
 import com.goldenPig.Result;
-import com.goldenPig.admin.dao.AdminDAO;
 import com.goldenPig.member.dao.MemberDAO;
 
-public class AdminMemberModifyController implements Action {
+public class AdminMemberModifyController implements Action{
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		AdminDAO adminDAO = new AdminDAO();
-		MemberDAO memberDAO = new MemberDAO();
-
 		Result result = new Result();
-		JSONArray jsons = new JSONArray();
-		
-		Map<String, Object> adminMemberModifyMap = new HashMap<String, Object>();
-		
-		
+		String uri = req.getRequestURI();
+		result.setPath("/templates/admin/adminPage-modifyMember.jsp");
+		result.setRedirect(FORWORD);
+		return result;
 	}
-
+	
+	
+	
 }
