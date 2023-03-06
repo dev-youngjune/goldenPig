093b44fa014c5326c38fe875ac516c81983d0eee
code-leesooncheol <2sooncheol@gmail.com>
2023-03-06 09:13:13 +0900

    멤버 VO 이미지 네임, 패스, 인사말 추가

--- src/com/goldenPig/mypage/MypageEditOkController.java
+++ src/com/goldenPig/mypage/MypageEditOkController.java
@@ -25,17 +25,17 @@ public class MypageEditOkController implements Action {
 		String nickname = (String) reqNickname;
 		
 		
-		/*
-		 * memberVO.setMemberNickName(req.getParameter("memberNickname"));
-		 * memberVO.setmemberGreeting(req.getParameter("memberGreeting"));
-		 * 
-		 * moneyVO.setMoneyTarget((int)req.getParameter("moneyTarget"));
-		 * moneyVO.setMoneyTarget(req.getParameter("moneyTarget"));
-		 * 
-		 * moneyVO.setMoneyAmount(req.getParameter("moneyAmount"));
-		 * moneyVO.setMoneyRegisterDate(req.getParameter("moneyRegister"));
-		 * moneyVO.setMoneyExpiryDate(req.getParameter("moneyExpiry"));
-		 */
+		
+//		memberVO.setMemberNickName(req.getParameter("memberNickname"));
+//		memberVO.setmemberGreeting(req.getParameter("memberGreeting"));
+//		
+//		moneyVO.setMoneyTarget(req.getParameter("moneyTarget"));
+//		moneyVO.setMoneyTarget(req.getParameter("moneyTarget"));
+//		
+//		moneyVO.setMoneyAmount(req.getParameter("moneyAmount"));
+//		moneyVO.setMoneyRegisterDate(req.getParameter("moneyRegister"));
+//		moneyVO.setMoneyExpiryDate(req.getParameter("moneyExpiry"));
+		
 		
 		
 //		모달프로필 멤버 정보 수정
